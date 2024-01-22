b54087e4e4b276e3b43f180b6a6cedf43f1139fe
Peter <netbka@gmail.com>
2024-01-22 10:42:04 +0200

    removed fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,20 +19,20 @@ export default defineNuxtConfig({
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         { rel: "preconnect", href: "https://fonts.gstatic.com" },
-        //{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto+Mono&display=swap" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto+Mono&display=swap" },
       ],
     },
   },
-  fontMetrics: {
-    fonts: ["DM Sans"],
-  },
-  googleFonts: {
-    display: "swap",
-    download: true,
-    families: {
-      "DM+Sans": [300, 400, 500, 600, 700],
-    },
-  },
+  // fontMetrics: {
+  //   fonts: ["DM Sans"],
+  // },
+  // googleFonts: {
+  //   display: "swap",
+  //   download: true,
+  //   families: {
+  //     "DM+Sans": [300, 400, 500, 600, 700],
+  //   },
+  // },
   vite: {
     plugins: [vsharp()],
   },
